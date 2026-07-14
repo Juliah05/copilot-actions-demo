f2e813da64049abae82d33ca7ae19a7eb4f97300
Julia05 <jh2499271@gmail.com>
2026-07-14 15:00:59 -0300

    Primeiro commit

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,13 +1,13 @@
 const calcularTotal = require('../src/calculator');
 
-test("10%", () => {
-   expect(calcularTotal(100,10)).toBe(90);
+test('Desconto de 10%', () => {
+    expect(calcularTotal(100, 10)).toBe(90);
 });
 
-test("Sem desconto", () => {
-   expect(calcularTotal(200,0)).toBe(200);
+test('Sem desconto', () => {
+    expect(calcularTotal(200, 0)).toBe(200);
 });
 
-test("50%", () => {
-   expect(calcularTotal(80,50)).toBe(40);
+test('Desconto de 50%', () => {
+    expect(calcularTotal(80, 50)).toBe(40);
 });
